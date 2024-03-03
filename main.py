5d6333dbef009c19935449280ab406ed0150de0f
Kisly93 <123402270+Kisly93@users.noreply.github.com>
2024-03-04 00:29:51 +0100

    added move rocket

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 import curses
 import random
 from fire_animation import fire
+from curses_tools import animate_spaceship
 
 TIC_TIMEOUT = 0.1
 
@@ -28,11 +29,20 @@ async def blink(canvas, row, column, symbol):
             await asyncio.sleep(0)
 
 
+def read_file(file_paths):
+    contents = []
+    for file_path in file_paths:
+        with open(file_path, 'r') as file:
+            contents.append(file.read())
+    return contents
+
+
 def draw(canvas):
     curses.curs_set(0)
     canvas.border('|', '|', '-', '-', '+', '+', '+', '+')
     max_y, max_x = canvas.getmaxyx()
     num_coroutines = random.randint(10, 50)
+    file_paths = ['animations/rocket_frame_1.txt', 'animations/rocket_frame_2.txt']
     coroutines = []
     for _ in range(num_coroutines):
         x = random.randint(1, max_x - 2)
@@ -40,9 +50,12 @@ def draw(canvas):
         symbol = random.choice('+*.:')
         coroutines.append(blink(canvas, y, x, symbol))
 
-    x = max_x // 2
-    y = max_y - 2
-    fire_coroutine = fire(canvas, y, x)
+    # x = max_x // 2
+    # y = max_y - 2
+    # fire_coroutine = fire(canvas, y , x)
+    frames = read_file(file_paths)
+    coroutine_spaceship = animate_spaceship(canvas, max_y - 1, max_x, frames)
+    coroutines.append(coroutine_spaceship)
 
     while True:
         for coroutine in coroutines:
@@ -50,10 +63,10 @@ def draw(canvas):
                 coroutine.send(None)
             except StopIteration:
                 coroutines.remove(coroutine)
-        try:
-            fire_coroutine.send(None)
-        except StopIteration:
-            pass
+        # try:
+        #   fire_coroutine.send(None)
+        # except StopIteration:
+        #   pass
 
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
