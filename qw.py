f7371cf364530e86d3f1c7a725097bcbb89cd1db
Kisly93 <123402270+Kisly93@users.noreply.github.com>
2024-03-03 20:58:23 +0100

    added stars animation

--- qw.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-async def count_to_three():
-    print("Веду отсчёт. 1")
-    await asyncio.sleep(0)
-    print("Веду отсчёт. 2")
-    await asyncio.sleep(0)
-    print("Веду отсчёт. 3")
-    await asyncio.sleep(0)
